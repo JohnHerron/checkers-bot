3e9c2ec8100df8f4e1b564939798a3673f8cb8c9
JohnHerron <herroncjohn@gmail.com>
2024-04-13 21:00:09 -0700

    ADD: pawns generated in board class and drawn on board

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from pawns import Pawn
 
 class Board:
     '''Board class represents an nxn game board used for display'''
@@ -6,17 +7,25 @@ class Board:
         self.window_surface = window_surface
         self.window_dims = window_surface.get_size()
         self.n_squares = n_squares
+        self.pawns = []
+
         # scale square board to 90% window height
         self.board_height = self.window_dims[1]*.9
         self.board = pg.Surface((self.board_height, self.board_height))
+
         # initialize Rect objs for squares on board
         self.generate_squares()
+        # initialize pawns on board with their locations
+        self.create_pawns()
 
     def draw(self):
         self.board.fill("pink")
         # draw square Rects to board
         [[pg.draw.rect(self.board, self.squares[x][y][0], self.squares[x][y][1])
            for x in range(self.n_squares)] for y in range(self.n_squares)]
+        
+        # draw pawns on board
+        [[pawn.draw() for pawn in row] for row in self.pawns]
 
         board_center = (
             (self.window_dims[0]-self.board.get_width())/2,
@@ -29,3 +38,11 @@ class Board:
         square_size = self.board.get_height()/self.n_squares
         self.squares = [[('white' if (x+y)%2 == 0 else 'black',pg.Rect(square_size * x, square_size * y, square_size, square_size)) 
                          for x in range(self.n_squares)] for y in range(self.n_squares)]
+        print(self.squares)
+        
+    def create_pawns(self):
+        num_rows = (self.n_squares//2) - 1
+        self.pawns = [[Pawn(self, square[item][1], 'darkgreen') for item in range(self.n_squares) if square[item][0] == 'black'] for square in self.squares[:num_rows]]
+        self.pawns += [[Pawn(self, square[item][1], 'red') for item in range(self.n_squares) if square[item][0] == 'black'] for square in self.squares[-num_rows:]]
+
+        print(self.pawns)
--- pawns.py
+++ pawns.py
@@ -1,9 +1,23 @@
-class Pawn:
-    def __init__(self, location, color) -> None:
-        self.king = False
-        self.location = location
-        self.color = color
-        self.moves = self.generate_moves()
+import pygame as pg
 
-    def move_to(self, new_location):
-        self.location = new_location
+class Pawn:
+    '''Pawns exist on an mxn board where their location is denoted by
+    an (y, x) or (h, w) pair where (0,0) is the top left of the board'''
+    def __init__(self, board, square, color, y, x) -> None:
+        self.board = board
+        self.king = False
+        self.square = square
+        self.color = color
+        self.x = x
+        self.y = y
+        # TODO:
+        # self.moves = self.generate_moves()
+
+    def draw(self):
+        pg.draw.circle(self.board.board, self.color, self.square.center, (self.square.height/2 * .7), 0)
+
+    def move_to(self, h, w):
+        '''Moves pawn to (h,w) on mxn board'''
+        self.square = self.board.squares[h][w]
+        self.y = h
+        self.x = w
