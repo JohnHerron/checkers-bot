829761ed2abbacd9a63d1679235758e4d614c11e
JohnHerron <herroncjohn@gmail.com>
2024-04-16 16:09:22 -0700

    ADD: checkers class calculates all possible moves

--- checkers.py
+++ checkers.py
@@ -10,15 +10,16 @@ class Checkers(Game):
     def __init__(self, screen, n_squares) -> None:
         self.board = Board(screen, n_squares)
         self.state = [[0] * n_squares for _ in range(n_squares)] # nxn 2d array filled with 0's
-        print(self.state)
         for row in self.board.pawns:
             for pawn in row:
-                self.state[pawn.y][pawn.x] = -1 if pawn.color == 'darkgreen' else 1
-        print(self.state)
-        self.moves = []
-        for pawn in self.board.pawns:
-            # surrounding spaces
-            pass
+                self.state[pawn.y][pawn.x] = 1 if pawn.color == 'red' else -1
+        self.moves = {} # dict of possible moves on board
+        for row_num, row in enumerate(self.state):
+            for col, tile_val in enumerate(row):
+                if tile_val:
+                    self.moves.update({f'{row_num},{col}': self.calculate_moves(row_num, col, tile_val)})
+        print(self.moves)
+                    
 
     def actions(self, node):
         '''Return list of valid moves from node'''
@@ -34,3 +35,27 @@ class Checkers(Game):
         '''Returns true if given node has no children (valid moves)'''
         return not self.moves
     
+    def calculate_moves(self, row, col, val, jumped = False):
+        '''Returns list of possible locations a piece on a give tile can move to.
+        If there is no pawn on tile, returns empty list.
+        Move format: [(y,x),(y,x)...]'''
+        valid_moves = []
+        if not val:
+            return []
+        front = row - val
+        left = col - val
+        right = col + val
+        # must check if diagonals are within bounds of board
+        if front >= 0 and front < self.board.n_squares:
+            if left >= 0 and left < self.board.n_squares:
+                if self.state[front][left] == 0:
+                    valid_moves.append((front, left))
+                elif self.state[front][left] != val:
+                    valid_moves.append(self.calculate_moves(front, left, val, True))
+            if right >= 0 and right < self.board.n_squares:
+                if self.state[front][right] == 0:
+                    valid_moves.append((front, right))
+                elif self.state[front][right] != val:
+                    valid_moves.append(self.calculate_moves(front, right, val, True))
+
+        return valid_moves
